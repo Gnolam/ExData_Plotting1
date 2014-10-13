b88c056b8ed63de086dc428b8ccb79b952f8028a
Alex Skorokhod <alex.skorokhod.com@gmail.com>
2014-10-13 13:59:42 +1100

    Changes in the X and Y axis labels
    This commit ensures axis labels to be 1:1 match with the assignment

--- Plot4.R
+++ Plot4.R
@@ -21,18 +21,36 @@ data$Datetime <- as.POSIXct(datetime)
 rm(datetime)
 
 
-# Create png file for Plot 3
-par(mfrow=c(1,1)) #, mar=c(4,4,2,1), oma=c(0,0,2,0))
+# Create png file for Plot 4
+
+par(mfrow=c(2,2))#, mar=c(4,4,2,1), oma=c(0,0,2,0))
+
 with(data, {
+
+  # Plot 1 - Copy
+  plot(Global_active_power~Datetime, type="l",ylab="Global active power",xlab="")
+
+  # Plot 2 - Copy
+  plot(Voltage~Datetime, type="l",
+       ylab="Voltage",xlab="datetime")
+
+
+  # Plot 3 - Copy
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
+
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+  # Plot 4 - New one
+  plot(Global_reactive_power~Datetime, type="l",xlab="datetime")
+
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
 
